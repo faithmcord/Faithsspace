15e3de5840b9bdbb4d3e6db82ba0f99a28b1a845
Faith Cordsiemon <142929626+faithmcord@users.noreply.github.com>
2023-08-25 18:09:34 +0000

    2

--- fun.h
+++ fun.h
@@ -1,3 +1,17 @@
 #include <cstdlib>
 #include <iostream>
+#include <string>
 #include <vector>
+
+class Rectangle
+{
+private:
+    double _width;
+    double _length;
+public:
+    explicit Rectangle(double width = 2, double length = 4);
+    {
+
+    }
+
+};
